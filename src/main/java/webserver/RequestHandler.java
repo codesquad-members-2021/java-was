923882f080b6107b75224635124b5048b1eeb6f6
gisungchoi <chlrltjd159@naver.com>
2021-03-22 17:15:43 +0900

    feat : request에서 url 읽기
    request 첫줄을 읽고 static파일을 응답으로 보내줄 수 있습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +22,18 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String buffer;
+            String[] getURL = null;
+            while (!((buffer = br.readLine()).equals(""))){
+                log.debug(buffer);
+                getURL = buffer.split(" ");
+                break;
+            }
+            byte[] body = Files.readAllBytes(new File("./webapp" + getURL[1]).toPath());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+//            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
