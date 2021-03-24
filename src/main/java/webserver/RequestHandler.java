6df281e5e2a594ddebb9abb049852bc0fe7d912a
tmdgusya <tmdgusya@naver.com>
2021-03-24 15:31:11 +0900

    [#6] feat: :cloud: get 방식으로 회원가입
    url 의 queryString 을 분석해서 User 객체를 생성하였음

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,19 +19,35 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    private Map<String, String> queryStrings = new HashMap<>();
+
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line = br.readLine();
             log.debug("line : {}", line);
             String[] s = line.split(" ");
             String url = s[1];
             log.info("url : {}", url);
+
+            if(url.startsWith("/user/create")) {
+                int i = url.indexOf('?');
+                String[] split = url.substring(i + 1, url.length()).split("&");
+                for(String query : split) {
+                    String[] ss = query.split("=");
+                    queryStrings.put(ss[0], ss[1]);
+                }
+                User user = new User(queryStrings.get("userId"), queryStrings.get("password"), queryStrings.get("name"), queryStrings.get("email"));
+                log.debug("user : {}", user);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
+
             byte[] body = Files.readAllBytes(new File("./webapp/"+url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
