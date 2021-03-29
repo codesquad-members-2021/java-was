ebc07f7c12093071ec1985aaf28052774394467e
gisungchoi <chlrltjd159@naver.com>
2021-03-29 16:51:15 +0900

    test : HttpResponse
    -response의 값들이 잘 들어갔는지 검사
    -static file 리턴 가능

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -1,11 +1,41 @@
 package http;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 public class HttpResponse {
+    private DataOutputStream dos;
+
     public HttpResponse(OutputStream os) {
+        dos = new DataOutputStream(os);
     }
 
-    public void forward(String url) {
+    public void forward(String url) throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+
+        if (url.endsWith(".css")) {
+            response200Header(dos, body.length, "css");
+        } else {
+            response200Header(dos, body.length, "html");
+        }
+
+        responseBody(dos, body);
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) throws IOException {
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/" + contentType + ";charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+
     }
 }
--- src/test/java/http/HttpResponseTest.java
+++ src/test/java/http/HttpResponseTest.java
@@ -2,9 +2,11 @@ package http;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import org.assertj.core.api.SoftAssertions;
 
@@ -25,14 +27,15 @@ class HttpResponseTest {
     }
 
     @Test
-    public void sendOk() {
+    public void sendOk() throws IOException {
         HttpResponse response = new HttpResponse(os);
         response.forward("/index.html");
 
-        softly.assertThat(os.toString()).contains("HTTP/1.1 200 OK\r\n");
+        softly.assertThat(os.toString()).contains("HTTP/1.1 200 OK \r\n");
         softly.assertThat(os.toString()).contains("Content-Type: text/html;charset=utf-8\r\n");
-        softly.assertThat(os.toString()).contains("Content-Length: 150\r\n");
+        softly.assertThat(os.toString()).contains("Content-Length: 7049\r\n");
         softly.assertThat(os.toString()).contains("<title>SLiPP Java Web Programming</title>");
     }
+
 
 }
