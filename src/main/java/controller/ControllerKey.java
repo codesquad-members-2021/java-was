7729bf19b192d7359ce6db42273681b35cfe281f
gisungchoi <chlrltjd159@naver.com>
2021-03-30 11:14:30 +0900

    feat : ControllerKey override equals hashCode
    -동일성을 보장한다

--- src/main/java/controller/ControllerKey.java
+++ src/main/java/controller/ControllerKey.java
@@ -2,9 +2,28 @@ package controller;
 
 import http.HttpMethod;
 
+import java.util.Objects;
+
 public class ControllerKey {
 
-    public ControllerKey(HttpMethod method, String url) {
+    private HttpMethod method;
+    private String url;
 
+    public ControllerKey(HttpMethod method, String url) {
+        this.method = method;
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ControllerKey that = (ControllerKey) o;
+        return method == that.method && Objects.equals(url, that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, url);
     }
 }
