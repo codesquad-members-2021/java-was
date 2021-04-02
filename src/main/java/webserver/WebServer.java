ef37ef5b47f949c8a0418eca64d03b53b198fc05
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2021-04-02 17:45:12 +0900

    refactor : HandlerAdapter
    -핸들러를 구성하는 맵을 만드는 어댑터를 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -17,29 +17,14 @@ public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    private static final Controller defaultController = new DefaultController();
-    private static final Map<ControllerKey, Controller> controllerMap = new HashMap<>();
-
-    static {
-
-        Reflections reflections = new Reflections("controller");
-        Set<Class<?>> requestMappingControllers = reflections.getTypesAnnotatedWith(RequestMapping.class);
-        for (Class<?> controller : requestMappingControllers) {
-            RequestMapping annotation = controller.getAnnotation(RequestMapping.class);
-            try {
-                controllerMap.put(new ControllerKey(annotation.method(), annotation.path()), (Controller) controller.newInstance());
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
 
     public static void main(String args[]) throws Exception {
         int port = DEFAULT_PORT;
         if (args != null && args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
+        HandlerAdapter adapter = new HandlerAdapter();
+
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -48,7 +33,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection, controllerMap, defaultController);
+                RequestHandler requestHandler = new RequestHandler(connection,adapter);
                 requestHandler.start();
             }
         }
